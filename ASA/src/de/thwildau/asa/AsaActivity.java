9eb819c43fa2ed2aabc7d259f6bb86a28f0454b6
FbnHffmnn <fbnhffmnn@gmail.com>
2013-12-15 17:42:10 +0100

    Map

--- ASA/src/de/thwildau/asa/AsaActivity.java
+++ ASA/src/de/thwildau/asa/AsaActivity.java
@@ -4,9 +4,15 @@ import android.app.Activity;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 
 public class AsaActivity extends Activity {
 	
+    // Google Map
+    private GoogleMap googleMap;
 	TextView asaHeaderTextView;
 	
 	 @Override
@@ -21,5 +27,26 @@ public class AsaActivity extends Activity {
 	        asaHeaderTextView.setTypeface(header_typeface);
 	        
 	 }
+
+	 
+	    /**
+	     * function to load map. If map is not created it will create it for you
+	     * */
+	    private void initilizeMap() {
+	        if (googleMap == null) {
+	            googleMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+	 
+	            // check if map is created successfully or not
+	            if (googleMap == null) {
+	                Toast.makeText(getApplicationContext(), "Sorry! unable to create maps", Toast.LENGTH_SHORT).show();
+	            }
+	        }
+	    }
+	    
+	    @Override
+	    protected void onResume() {
+	        super.onResume();
+	        initilizeMap();
+	    }
 
 }
